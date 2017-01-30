986d843ab77b5abc321d97b6390a253529b2608b
omgjwood <justinemwood@gmail.com>
2017-01-30 01:23:31 -0600

    found a js file to help me through the way.  this is confusing

--- calculator.js
+++ calculator.js
@@ -5,3 +5,42 @@
  */
 
 
+// Get all the keys from document
+var keys = document.querySelectorAll('#calculator span');
+var operators = ['+', '-', 'x', '÷'];
+var decimalAdded = false;
+
+// Add onclick event to all the keys and perform operations
+for(var i = 0; i < keys.length; i++) {
+    keys[i].onclick = function (e) {
+        // Get the input and button values
+        var input = document.querySelector('.screen');
+        var inputVal = input.innerHTML;
+        var btnVal = this.innerHTML;
+
+        // Now, just append the key values (btnValue) to the input string and finally use javascript's eval function to get the result
+        // If clear key is pressed, erase everything
+        if (btnVal == 'C') {
+            input.innerHTML = '';
+            decimalAdded = false;
+        }
+
+        // If eval key is pressed, calculate and display the result
+        else if (btnVal == '=') {
+            var equation = inputVal;
+            var lastChar = equation[equation.length - 1];
+
+            // Replace all instances of x and ÷ with * and / respectively. This can be done easily using regex and the 'g' tag which will replace all instances of the matched character/substring
+            equation = equation.replace(/x/g, '*').replace(/÷/g, '/');
+
+            // Final thing left to do is checking the last character of the equation. If it's an operator or a decimal, remove it
+            if (operators.indexOf(lastChar) > -1 || lastChar == '.')
+                equation = equation.replace(/.$/, '');
+
+            if (equation)
+                input.innerHTML = eval(equation);
+
+            decimalAdded = false;
+        }
+
+    }
